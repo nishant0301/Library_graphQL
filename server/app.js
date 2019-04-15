b3fa42b8fdc03147e13a5f83cac03a777c61da41
Ankit Pahwa <ankitpahwa111@gmail.com>
2019-04-16 02:58:40 +0530

    <>

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express();
+const path = require('path');
+app.use(cors())
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, '../client/build','index.html'))
+  })
+mongoose.connect('mongodb+srv://ankit9899:ankit9899@libraryankit-wuabp.mongodb.net/test?retryWrites=true')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql : true,
+}));
+const normalizePort = port => parseInt(port,10);
+const PORT = normalizePort(process.env.PORT || 5000)
+
+app.listen(PORT, () => {
+    console.log('now listening for requests on port 4000');
+});
